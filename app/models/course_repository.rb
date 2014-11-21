e77483510634de597abd81ebae7019072a8ec005
Ian Whitney <whit0694@umn.edu>
2014-11-21 16:07:40 -0600

    Alias LiberalEducationClasses as CourseRepository
    A bunch of code talks about CourseRepository, which no longer really
    exists. For now I'm just going to alias the class that does the work
    (LiberalEducationClasses) as the class that the rest of the app expects
    (CourseRepository)

--- app/models/course_repository.rb
+++ app/models/course_repository.rb
@@ -1,17 +1,1 @@
-class CourseRepository < BasicObject
-  def self.all(_=nil)
-    #noop
-  end
-
-  def self.writing_intensive(_)
-    #noop
-  end
-
-  def self.designated_theme(theme)
-    #noop
-  end
-
-  def self.diversified_core(theme)
-    #noop
-  end
-end
+CourseRepository = LiberalEducationCourse
