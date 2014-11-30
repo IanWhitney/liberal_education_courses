0338782dbdc3eb697b202b34d9d52a3ed6a40f2f
Ian Whitney <whit0694@umn.edu>
2014-11-30 08:36:34 -0600

    Add some tests around the current liberale_education_course

---
+++ spec/models/liberal_education_course_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../../app/models/liberal_education_course"
+require_relative "../../app/models/search_parameter"
+require_relative "../../app/models/cached_course_repository"
+
+RSpec.describe LiberalEducationCourse do
+  before do
+    allow(CachedCourseRepository).to receive(:empty?).and_return(false)
+  end
+
+  describe "single filters" do
+    it "search the repo with just one parameter" do
+      [:writing_intensive, :designated_theme, :diversified_core, :subject].each do |filter|
+        search_param_double = double("SearchParameter")
+        rand_result = rand
+        expect(SearchParameter).to receive(:new).with(filter, "WI").and_return(search_param_double)
+        expect(CachedCourseRepository).to receive(:query).with(search_param_double).and_return(rand_result)
+        expect(LiberalEducationCourse.public_send(filter, "WI")).to eq(rand_result)
+      end
+    end
+  end
+end
