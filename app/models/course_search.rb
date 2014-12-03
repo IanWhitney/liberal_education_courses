2000d6c713df5ae13fe21423d09ebd289d06fe52
Ian Whitney <whit0694@umn.edu>
2014-12-03 07:08:34 -0600

    Provide CourseSearch a collection of search_parameters
    CourseSearch had been getting raw query strings and then parsing them.
    Now it just gets a collection of search parameter objects that it can
    pass on to its search target.
    
    It does check to see if the search_params want to return all, in which
    case it calls the target's all method. This could be better.

--- app/models/course_search.rb
+++ app/models/course_search.rb
@@ -1,6 +1,9 @@
 class CourseSearch
-  def self.search(params = nil, search_target = LiberalEducationCourse)
-    search_params = QueryParser.parse(params)
-    search_target.where(search_params)
+  def self.search(search_params = nil, search_target = LiberalEducationCourse)
+    if search_params.all?
+      search_target.all
+    else
+      search_target.where(search_params)
+    end
   end
 end
--- spec/models/course_search_spec.rb
+++ spec/models/course_search_spec.rb
@@ -1,35 +1,26 @@
 require_relative "../../app/models/course_search"
-require_relative "../../app/models/query_parser"
+require_relative "../../app/models/search_parameters"
 require_relative "../../app/models/liberal_education_course"
 
 RSpec.describe CourseSearch do
   let(:search_target) { double("LiberalEducationCourse") }
-  let(:courses_double) { Object.new }
-  let(:parsed_query) { instance_double("QueryParser") }
+  let(:search_params) { double("SearchParameters") }
 
-  before do
-    allow(parsed_query).to receive(:search_type).and_return("".to_sym)
-    allow(parsed_query).to receive(:search_param)
+  describe "search is for all" do
+    it "returns the search_target.all results" do
+      rand_result = rand
+      allow(search_params).to receive(:all?).and_return(true)
+      expect(search_target).to receive(:all).and_return(rand_result)
+      expect(CourseSearch.search(search_params, search_target)).to eq(rand_result)
+    end
   end
 
-  describe "search class method" do
-    describe "given no parameters" do
-      it "returns LiberalEducationCourse.all" do
-        allow(parsed_query).to receive(:search_type).and_return(:all)
-        expect(search_target).to receive(:where).and_return(courses_double)
-        expect(QueryParser).to receive(:parse).with(nil).and_return(parsed_query)
-        results = CourseSearch.search(nil, search_target)
-
-        expect(results).to equal(courses_double)
-      end
-    end
-
-    describe "given parameters" do
-      it "uses the parsed parameters to query LiberalEducationCourse" do
-        expect(QueryParser).to receive(:parse).with("writing_intensive=true").and_return(writing_intensive: "true")
-        expect(search_target).to receive(:where).with(writing_intensive: "true")
-        CourseSearch.search("writing_intensive=true", search_target)
-      end
+  describe "search is not for all" do
+    it "returns the search_target.where results" do
+      rand_result = rand
+      allow(search_params).to receive(:all?).and_return(false)
+      expect(search_target).to receive(:where).with(search_params).and_return(rand_result)
+      expect(CourseSearch.search(search_params, search_target)).to eq(rand_result)
     end
   end
 end
