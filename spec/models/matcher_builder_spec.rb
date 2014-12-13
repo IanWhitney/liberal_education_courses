fd56fbd7abf98f27e411e5a01167eda1f48b4191
Ian Whitney <whit0694@umn.edu>
2014-12-12 22:10:51 -0600

    Removing the MatcherBuilder tests for now

--- spec/models/matcher_builder_spec.rb
+++ spec/models/matcher_builder_spec.rb
@@ -1,30 +1,5 @@
 require_relative "../../app/models/matcher_builder"
 
 RSpec.describe MatcherBuilder do
-  #describe "parse" do
-    #it "creates an instance of query parser" do
-      #returned = QueryParser.parse("test=hello")
-      #expect(returned).to eq(test: "HELLO")
-    #end
-  #end
-
-  #describe "new" do
-    #describe "with a unparsed query" do
-      #let(:unparsed_query) { "test=hello" }
-
-      #it "sets its properties from the query components" do
-        #it = QueryParser.new("test=hello")
-        #expect(it.search_type).to eq(:test)
-        #expect(it.search_param).to eq("HELLO")
-      #end
-    #end
-
-    #describe "without a writing intensive query" do
-      #it "converts the search_param to WI" do
-        #it = QueryParser.new("writing_intensive=#{rand}")
-        #expect(it.search_type).to eq(:writing_intensive)
-        #expect(it.search_param).to eq("WI")
-      #end
-    #end
-  #end
+  it "is not yet tested"
 end
