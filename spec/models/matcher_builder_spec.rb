4affcc80862d7f366512bf89e38e8b30bc42ded3
Ian Whitney <whit0694@umn.edu>
2014-12-14 11:01:45 -0600

    Simplify MatcherBuilder tests
    These tests reflected the old implementation. Updating to test what we
    actually need to test in this class.

--- spec/models/matcher_builder_spec.rb
+++ spec/models/matcher_builder_spec.rb
@@ -1,65 +1,32 @@
 require_relative "../../app/models/matcher_builder"
 require_relative "../../app/models/match_attribute"
-require_relative "../../app/models/match_no_attribute"
-require_relative "../../app/models/match_attribute_value"
+require_relative "../../app/models/search_parameter"
+require_relative "../../app/models/query_options"
 
 RSpec.describe MatcherBuilder do
+  let(:matcher_double) { class_double("MatchAttribute") }
+  let(:param_double) { double("QueryOptions") }
+
+  before do
+    allow(SearchParameter).to receive(:matchers).and_return([matcher_double])
+    allow(param_double).to receive(:search_param).and_return("test_search_param")
+    allow(param_double).to receive(:search_type).and_return("test_search_type")
+  end
+
   describe "build" do
-    describe "no parameters provided" do
-      it "returns a MatchAll" do
-        match_double = Object.new
-        expect(MatchAll).to receive(:new).and_return(match_double)
-        it = MatcherBuilder.build
-        expect(it).to eq(match_double)
+    describe "finds a matcher to build" do
+      it "builds that matcher and returns it" do
+        test_return = Object.new
+        expect(matcher_double).to receive(:build_me?).with(param_double.search_type, param_double.search_param).and_return(true)
+        expect(matcher_double).to receive(:new).with(param_double.search_type, param_double.search_param).and_return(test_return)
+        expect(MatcherBuilder.build(param_double)).to eq(test_return)
       end
     end
 
-    describe "reserved-word parameter provided" do
-      describe "parameter is all" do
-        it "returns a MatchAttribute" do
-          match_double = Object.new
-          expect(MatchAttribute).to receive(:new).with(:something, "all").and_return(match_double)
-          it = MatcherBuilder.build("something=all")
-          expect(it).to eq(match_double)
-        end
-      end
-
-      describe "parameter is none" do
-        it "returns a MatchNoAttribute" do
-          match_double = Object.new
-          expect(MatchNoAttribute).to receive(:new).with(:something, "none").and_return(match_double)
-          it = MatcherBuilder.build("something=none")
-          expect(it).to eq(match_double)
-        end
-      end
-    end
-
-    describe "boolean parameter provided" do
-      describe "parameter is true" do
-        it "returns a MatchAttribute" do
-          match_double = Object.new
-          expect(MatchAttribute).to receive(:new).with(:something, "true").and_return(match_double)
-          it = MatcherBuilder.build("something=true")
-          expect(it).to eq(match_double)
-        end
-      end
-
-      describe "parameter is false" do
-        it "returns a MatchNoAttribute" do
-          match_double = Object.new
-          expect(MatchNoAttribute).to receive(:new).with(:something, "false").and_return(match_double)
-          it = MatcherBuilder.build("something=false")
-          expect(it).to eq(match_double)
-        end
-      end
-    end
-
-    describe "string parameter provided" do
-      it "returns a MatchAttributeValue" do
-        match_double = Object.new
-        expect(MatchAttributeValue).to receive(:new).with(:something, "value").and_return(match_double)
-        it = MatcherBuilder.build("something=value")
-        expect(it).to eq(match_double)
+    describe "does not find a matcher to build" do
+      it "returns nil" do
+        expect(matcher_double).to receive(:build_me?).and_return(false)
+        expect(MatcherBuilder.build(param_double)).to be_nil
       end
     end
   end
